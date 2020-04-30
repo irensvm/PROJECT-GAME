a83a14379c75beebc4cec9d815b087e4e12d5120
irene villanueva <irens.vm@gmail.com>
2020-04-30 21:03:54 +0200

    movingbutcanvasdissapear

--- canvas.js
+++ canvas.js
@@ -17,11 +17,11 @@
         img: null,
         x: 300,
         y: 300,
-        speed: 30,
+        speed: 100,
         size: 100,
         loadImg: function () {
             this.img = new Image();
-            this.img.src = "images/player.png"
+            this.img.src = "images/player2.png"
             this.img.onload = () => {
                 ctx.drawImage(this.img, this.x, this.y, this.size,this.speed );
             
@@ -35,6 +35,13 @@
         player.loadImg();
         
     }
+    function updateCanvas() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+             
+        ctx.drawImage(player.img, player.x, player.y);
+        ctx.drawImage(canvasBckgr.img,0,0);
+      }
+      
   
 
     window.onload = () => {
@@ -43,9 +50,37 @@
         };
     }
 
+    function playerMove(event) {
+        if(event.code == "ArrowRight"){
+            player.x += player.speed;
+        console.log("dcha");
+        }
+        if(event.code == "ArrowLeft"){
+            player.x -= player.speed;
+            console.log("izqda");
 
 
-    
+        }
+      // if(player.x >= canvas.width - 50) {
+      //     player.x = canvas.width - 50;
+      //   } else if (player.x <= 0) {
+      //     player.x = 0;
+      //   }
+      //   if (player.y <= 0) {
+      //     player.y = 0;
+      //   } else if(player.y >= canvas.height - 300) {
+      //     player.y = canvas.height - 300;
+      //   }
+      
+          updateCanvas();
+        
+    }
+    document.onkeydown = playerMove; 
+
+
+
+
+
 
 
    //const leftButton = new Image();
