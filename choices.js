b1da935d249bce2eb2a9bff24b8870fe6b2d0e49
irene villanueva <irens.vm@gmail.com>
2020-05-07 08:59:54 +0200

    imagesaddedagain

--- choices.js
+++ choices.js
@@ -6,61 +6,76 @@ const choicesArray = [{
     {
         name: "Eat some waffles, you deserve them",
         points: 15,
+        img: "/images/choices/waffles.png "
     },
     {
         name: "Drink fancy & healthy smoothie",
         points: 15,
+        img: "/images/choices/smoothie.png "
     },
     {
         name: "Attend a Webinar",
         points: 5,
+        img: "/images/choices/webinar.png "
     },
     {
         name: "Ironbeers",
         points: 15,
+        img: "/images/choices/ironbeers.png "
     },
     {
         name: "Create a manual of good practices",
         points: 5,
+        img: "/images/choices/manual.png "
     },
     {
         name: "Join a Hackathon",
         points: 5,
+        img: "/images/choices/hackathon.png "
     },
     {
         name: "Extend your Canvas knowledge",
         points: -5,
+        img: "/images/choices/canvas.svg "
     },
     {
         name: "Review your first LAB: smiles and tears",
         points: 5,
+        img: "/images/choices/lab.png "
     },
     {
         name: "Research, read and stay updated",
         points: 7,
+        img: "/images/choices/research.png  ",
     },
     {
         name: "Buy and read the books that every good programmer should read throughout his life.",
         points: 5,
+        img: "/images/choices/books.png ",
     },
     {
         name: "Persevere, don't give up, and learn another programming language",
         points: 7,
+        img: "/images/choices/lab.png ",
     },
     {
         name: "Plan to nap this Sunday",
         points: 25,
+        img: "/images/choices/nap.png",
     },
     {
         name: "Do some code review",
         points: 7,
+        img: "/images/choices/codereview.png",
     },
     {
         name: "Stop and take a deep breath",
         points: 25,
+        img: "/images/choices/breath.png",
     },
     {
         name: "Look for some developer pranks",
         points: 5,
+        img: "/images/choices/prank.png"
     },
 ];
